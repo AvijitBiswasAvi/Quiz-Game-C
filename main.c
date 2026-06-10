9326cf99b6a07bcc479ce2c5a158bba999b44e10
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-06-10 11:15:01 +0600

    update quiz game

--- main.c
+++ main.c
@@ -4,7 +4,18 @@ int main() {
     int choice;
     do
     {
-        
+        printf("\n");
+        printf("|=====================|\n");
+        printf("|        MENU         |\n");
+        printf("|---------------------|\n");
+        printf("| 1. Start Quiz       |\n");
+        printf("| 2. View Rules       |\n");
+        printf("| 3. Exit             |\n");
+        printf("|=====================|\n");
+
+        printf("Enter your choice = ");
+        scanf("%d", &choice);
+  
     } while (choice != 3);
     
     return 0;
